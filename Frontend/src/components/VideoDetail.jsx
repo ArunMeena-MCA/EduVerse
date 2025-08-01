d5daa85fc8368154b6ffb7372f013bf7103ee37a
Arun Meena <Arunmeena.mca@gmail.com>
2025-08-01 23:49:13 +0530

    Updated

--- Frontend/src/components/VideoDetail.jsx
+++ Frontend/src/components/VideoDetail.jsx
@@ -305,7 +305,7 @@ function VideoDetail() {
       <div className="md:w-[64%] ml-0 md:ml-12">
         <div className="mt-4">
           <video
-            className="rounded flex justify-center items-center bg-gray-900 h-[400px] w-full"
+            className="rounded flex justify-center items-center bg-gray-900 h-[500px] w-full"
             src={video.videoFile}
             controls
             autoPlay
